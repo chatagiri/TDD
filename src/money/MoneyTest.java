622852decce7b4ba48f71079908cc117344f43f6
chatagiri <chatagiriii@gmail.com>
2018-04-04 00:23:37 +0900

    p14

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -3,13 +3,16 @@ package money;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-// page 11
+// page 14
 public class MoneyTest {
 
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
