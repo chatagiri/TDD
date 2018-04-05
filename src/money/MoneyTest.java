46bda964accd2f02da414de739b3376fa627df0b
chatagiri <chatagiriii@gmail.com>
2018-04-05 22:03:35 +0900

    p26-3

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -8,10 +8,11 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product);
-        product = five.times(3);
-        assertEquals(new Dollar(15), product);
+        // 一時変数productは不要となった
+        // Dollar product = five.times(2);
+        assertEquals(new Dollar(10), five.times(2));
+        // product = five.times(3);
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
